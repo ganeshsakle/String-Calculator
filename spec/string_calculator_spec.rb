deb294c6f1da61c1d5d1834f6b05bf591f810eab
ganeshsakle <ganeshsakle7@gmail.com>
2025-04-03 14:39:07 +0530

    Test case for sum of n number separated by comma

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,22 +3,26 @@ require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
   describe '#add' do
+    let(:string_calculator) { StringCalculator.new }
+
     it 'returns 0 for an empty string' do
-      string_calculator = StringCalculator.new
       result = string_calculator.add('')
       expect(result).to eq(0)
     end
 
     it 'returns the actual number if just one value is supplied' do
-      string_calculator = StringCalculator.new
       result = string_calculator.add('1')
       expect(result).to eq(1)
     end
 
     it 'calculates the total of two numbers if they are separated by a comma' do
-      string_calculator = StringCalculator.new
       result = string_calculator.add('1,2')
       expect(result).to eq(3)
+    end
+
+    it 'returns the sum of n number separated by comma' do
+      result = string_calculator.add('1,2,3')
+      expect(result).to eq(6)
     end
   end
 end
