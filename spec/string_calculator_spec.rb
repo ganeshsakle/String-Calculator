d4bae234e46a3cf91187a938d985cbfd37db19df
ganeshsakle <ganeshsakle7@gmail.com>
2025-04-03 14:25:00 +0530

    Test case for single digit number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe StringCalculator do
       result = string_calculator.add('')
       expect(result).to eq(0)
     end
+
+    it 'returns the actual number if just one value is supplied' do
+      string_calculator = StringCalculator.new
+      result = string_calculator.add('1')
+      expect(result).to eq(1)
+    end
   end
 end
