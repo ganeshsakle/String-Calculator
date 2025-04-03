df4f6d3d418deb3506c691c207f5e2802ae56bc3
ganeshsakle <ganeshsakle7@gmail.com>
2025-04-03 15:00:17 +0530

    Test case for new line char and code refactor

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,29 +5,40 @@ RSpec.describe StringCalculator do
   describe '#add' do
     let(:string_calculator) { StringCalculator.new }
 
-    it 'returns 0 for an empty string' do
-      result = string_calculator.add('')
-      expect(result).to eq(0)
+    context "Test case of an empty string, one & two numbers" do
+      it 'returns 0 for an empty string' do
+        result = string_calculator.add('')
+        expect(result).to eq(0)
+      end
+
+      it 'returns the actual number if just one value is supplied' do
+        result = string_calculator.add('1')
+        expect(result).to eq(1)
+      end
+
+      it 'calculates the total of two numbers if they are separated by a comma' do
+        result = string_calculator.add('1,2')
+        expect(result).to eq(3)
+      end
     end
 
-    it 'returns the actual number if just one value is supplied' do
-      result = string_calculator.add('1')
-      expect(result).to eq(1)
+    context "Allow the Add method to handle an unknown amount of numbers" do
+      it 'returns the sum of n number separated by comma' do
+        result = string_calculator.add('1,2,3')
+        expect(result).to eq(6)
+      end
     end
 
-    it 'calculates the total of two numbers if they are separated by a comma' do
-      result = string_calculator.add('1,2')
-      expect(result).to eq(3)
-    end
+    context "Allow the Add method to handle new lines between numbers" do
+      it 'returns the sum when number has new lines char between numbers' do
+        result = string_calculator.add('1\n2,3')
+        expect(result).to eq(6)
+      end
 
-    it 'returns the sum of n number separated by comma' do
-      result = string_calculator.add('1,2,3')
-      expect(result).to eq(6)
-    end
-
-    it 'returns the sum when number has new lines char between numbers' do
-      result = string_calculator.add('1\n2,3')
-      expect(result).to eq(6)
+      it 'returns the INVALID input so do not expect it : “1,\n”' do
+        result = string_calculator.add('1,\n')
+        expect(result).to eq('Not Allowed')
+      end
     end
   end
 end
