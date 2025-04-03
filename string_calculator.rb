3d8d3a798b9b9d23f7a1e5741a392cee58568ee3
ganeshsakle <ganeshsakle7@gmail.com>
2025-04-03 15:05:53 +0530

    Logic to return Not allowed for new line char

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,8 @@ class StringCalculator
     if input.chars.count <= 1
       input.empty? ? 0 : input.to_i
     else
+      return 'Not Allowed' if input.include?(',\n')
+
       arr = []
       input.delete(', ').chars.each do |char|
         arr.push(char.to_i)
