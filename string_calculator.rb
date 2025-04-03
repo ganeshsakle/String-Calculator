de7d7725e2427db10e6f8af03a039d064d54b2d1
ganeshsakle <ganeshsakle7@gmail.com>
2025-04-03 14:34:57 +0530

    Code for two number separated by comma

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,14 @@
 class StringCalculator
   def add(input)
-    input.empty? ? 0 : input.to_i
+    if input.chars.count <= 1
+      input.empty? ? 0 : input.to_i
+    else
+      arr = []
+      input.delete(', ').chars.each do |char|
+        arr.push(char.to_i)
+      end
+
+      arr.sum
+    end
   end
 end
